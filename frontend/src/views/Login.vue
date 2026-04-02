209e4d29dbee87c3888e8a2f71229a42eb54f49a
y7yun <3112956996@qq.com>
2026-04-02 19:35:47 +0800

    Add Login.vue as login form page

---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="login">
+    <h1>Login</h1>
+    <form @submit.prevent="handleLogin">
+      <div>
+        <label for="username">Username:</label>
+        <input type="text" id="username" v-model="username" required />
+      </div>
+      <div>
+        <label for="password">Password:</label>
+        <input type="password" id="password" v-model="password" required />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      password: ''
+    };
+  },
+  methods: {
+    handleLogin() {
+      // Handle login logic
+      console.log('Username:', this.username);
+      console.log('Password:', this.password);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.login {
+  max-width: 400px;
+  margin: 0 auto;
+  padding: 1em;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.1);
+}
+
+h1 {
+  text-align: center;
+}
+
+form div {
+  margin-bottom: 1em;
+}
+
+label {
+  margin-bottom: .5em;
+  color: #333333;
+  display: block;
+}
+
+input {
+  border: 1px solid #CCCCCC;
+  padding: .5em;
+  width: 100%;
+  box-sizing: border-box;
+  border-radius: 5px;
+}
+
+button {
+  padding: 0.7em;
+  color: #FFFFFF;
+  background-color: #007BFF;
+  border: none;
+  border-radius: 5px;
+}
+</style>
