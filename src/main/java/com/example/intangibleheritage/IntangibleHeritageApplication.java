a88f07a6acb8653d2c93368f3b4afba10c2a314f
y7yun <3112956996@qq.com>
2026-04-02 19:09:01 +0800

    Add IntangibleHeritageApplication.java

---
+++ src/main/java/com/example/intangibleheritage/IntangibleHeritageApplication.java
@@ -0,0 +1,13 @@
+package com.example.intangibleheritage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntangibleHeritageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntangibleHeritageApplication.class, args);
+    }
+
+}
